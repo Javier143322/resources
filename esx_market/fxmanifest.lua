6d46bddf60a5ad7d211b7b9e2e3759f5b12b515a
Javier143322 <huhdnuhdl@gmail.com>
2025-10-19 16:22:30 -0500

    Actualizar fxmanifest.lua

--- esx_market/fxmanifest.lua
+++ esx_market/fxmanifest.lua
@@ -1,1 +1,31 @@
+fx_version 'cerulean'
+game 'gta5'
 
+author 'TuNombre'
+description 'Mega Mercado - Sistema de Venta de Alimentos'
+version '1.0.0'
+
+ui_page 'html/index.html'
+
+client_scripts {
+    'config.lua',
+    'client/main.lua'
+}
+
+server_scripts {
+    '@mysql-async/lib/MySQL.lua',
+    'config.lua',
+    'server/main.lua'
+}
+
+files {
+    'html/index.html',
+    'html/css/style.css',
+    'html/js/script.js',
+    'html/images/*.png'
+}
+
+dependencies {
+    'es_extended',
+    'mysql-async'
+}
